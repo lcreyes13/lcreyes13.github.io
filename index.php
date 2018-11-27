d4a1b56109569851575d9b4fdc1ea9e8bb1f93ff
Patrick Reyes <watafungya@gmail.com>
2018-11-27 13:23:16 -0800

    Add Updates for index php
    Signed-off-by: Patrick Reyes <watafungya@gmail.com>

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-<h1>wakanda poreber</h1>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>JoJoBytes</title>
+    <meta name="Speedtest">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <style>
+        /* width */
+        ::-webkit-scrollbar {
+            width: 5px;
+            height: 5px;
+            border-bottom-left-radius: 10px;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: #888;
+            border-radius: 10px;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #555;
+        }
+    </style>
+    <link href="scripts/flipclock.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="scripts/font-awesome.min.css">
+</head>
+<body>
+<h4 class="text-center"> Welcome To JoJoBytes Website </h4>
+
+
+
+<script type="text/javascript" src="scripts/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="scripts/moment.min.js"></script>
+<script type="text/javascript" src="scripts/moment-timezone-with-data.min.js"></script>
+</body>
+</html>
